9711b8b4eda27e2277eb7f7b8410b066b0f7cf36
Derick Castillo <derick.castillo@flatironschool.com>
2021-01-13 22:35:06 -0600

    created JSON backend for tiles

--- index.js
+++ index.js
@@ -16,3 +16,49 @@ function displayForm() {
 	}
 }
 
+function fetchGoals(){
+	fetch("http://localhost:3000/goals")
+	.then(response => response.json())
+	.then(goalList => {
+		goalList.forEach(item => showItem(item))
+	})
+}
+
+function showItem(itemData){
+	// console.log(itemData)
+	// CREATE ELEMENTS
+	const outerDiv = document.createElement("div")
+	const innerDiv = document.createElement("div")
+	const addBtn = document.createElement("button")
+	const deleteBtn = document.createElement("button")
+	const hashBtn = document.createElement("button")
+	const h1 = document.createElement("h1")
+	const p = document.createElement("p")
+
+	// GRAB ELEMENT
+	const divTileContainer = document.querySelector("#tileContainerId")
+
+	// ADD ATTRIBUTES TO ELEMENTS
+	outerDiv.setAttribute("class", "card")
+	outerDiv.setAttribute("style", "width: 18rem")
+	innerDiv.setAttribute("class", "card-body justify-content-center")
+	addBtn.setAttribute("class", "btn btn-successs")
+	addBtn.setAttribute("style", "margin: 4px")
+	addBtn.innerText = "Add"
+	deleteBtn.setAttribute("class", "btn btn-danger")
+	deleteBtn.setAttribute("style", "margin: 4px")
+	deleteBtn.innerText= "Delete"
+	hashBtn.setAttribute("class", "btn btn-secondary")
+	hashBtn.innerText = itemData.category
+	hashBtn.setAttribute("style", "margin: 4px")
+	h1.innerText = itemData.goal
+	p.innerText = itemData.description
+
+	// CONNECT ALL THE ELEMENTS
+	outerDiv.append(innerDiv)
+	innerDiv.append(addBtn, deleteBtn, hashBtn, h1, p)
+	divTileContainer.append(outerDiv)
+
+}
+
+fetchGoals()
